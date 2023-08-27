68a21af2badfaead87152e196dd3d76f579e38bc
gka0903 <ham3252@naver.com>
2023-08-27 14:41:10 +0900

    interface 분리 질문 상세 화면 테스트

---
+++ src/noticeBoard/interface.tsx
@@ -0,0 +1,9 @@
+// Question.ts
+interface Question {
+    id: number;
+    subject: string;
+    content: string;
+    create_date: number;
+}
+
+export default Question;
--- src/noticeBoard/page/Detail.tsx
+++ src/noticeBoard/page/Detail.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import {useParams} from "react-router-dom";
+import fastAPI from "../../lib/api";
+import Question from "../interface";
+
 
 const Container = styled.div``;
 
 const Detail = () => {
     const {question_id} = useParams();
-    console.log("question_id: " + question_id);
+    const [question, setQuestion] = useState<Question | null>();
 
+    function get_question() {
+        fastAPI("get", "/question/detail/" + question_id, {}, (json) => {
+            setQuestion(json)
+        })
+    }
+
+    useEffect(() => {
+        get_question();
+    }, []);
+    
     return (
         <Container>
-            <h1>제목</h1>
-            <div>내용</div>
+            <h1>{question ? question.subject : ""}</h1>
+            <div>{question ? question.content : ""}</div>
         </Container>
     );
 };
